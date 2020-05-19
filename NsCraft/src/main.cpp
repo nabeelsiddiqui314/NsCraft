756be165a2c6823e543b7afeeeba089fd9d764a7
Nabeel Siddiqui <nabeel.siddiqui.314@gmail.com>
2020-05-20 00:16:37 +0530

    Included opengl and sfml.

--- NsCraft/src/main.cpp
+++ NsCraft/src/main.cpp
@@ -1,6 +1,36 @@
-#include <iostream>
+#include <SFML/Window.hpp>
+#include <GL/glew.h>
 
 int main() {
-	std::cin.get();
-	return 0;
+	sf::ContextSettings settings;
+	settings.depthBits = 24;
+	settings.stencilBits = 8;
+	settings.antialiasingLevel = 4;
+	settings.majorVersion = 4;
+	settings.minorVersion = 6;
+
+	sf::Window window(sf::VideoMode(1280, 720), "NsCraft", sf::Style::Default, settings);
+	window.setActive(true);
+
+	glewExperimental = GL_TRUE;
+
+	if (GLEW_OK != glewInit()) {
+		return EXIT_FAILURE;
+	}
+
+	while (window.isOpen()) {
+		sf::Event evnt;
+		while (window.pollEvent(evnt)) {
+			if (evnt.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+
+		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		window.display();
+	}
+
+	return EXIT_SUCCESS;
 }
