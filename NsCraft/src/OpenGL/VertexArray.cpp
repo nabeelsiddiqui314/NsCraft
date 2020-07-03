742681f95f4ecd29153ee1ef1769e1db140a3439
Nabeel Siddiqui <nabeel.siddiqui.314@gmail.com>
2020-07-03 20:49:22 +0530

    fixed incorrect vertex attribute index bug.

--- NsCraft/src/OpenGL/VertexArray.cpp
+++ NsCraft/src/OpenGL/VertexArray.cpp
@@ -14,12 +14,12 @@ void VertexArray::addVertexBuffer(const std::shared_ptr<VertexBuffer>& vertexBuf
 	bind();
 	vertexBuffer->bind();
 
+	m_vertexBuffers.push_back(vertexBuffer);
+
 	GLuint index = m_vertexBuffers.size() - 1;
 
 	glEnableVertexAttribArray(index);
 	glVertexAttribPointer(index, size, GL_FLOAT, GL_FALSE, 0, (void*)0);
-
-	m_vertexBuffers.push_back(vertexBuffer);
 }
 
 void VertexArray::setIndexBuffer(const std::shared_ptr<IndexBuffer>& indexBuffer) {
