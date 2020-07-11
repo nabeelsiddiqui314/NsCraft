3686855cd96490afe49f5aebb606ccae8cfc0fb8
Nabeel Siddiqui <nabeel.siddiqui.314@gmail.com>
2020-07-11 20:49:02 +0530

    Made blocks agnostic to names and ids

--- NsCraft/src/World/Blocks/Block.h
+++ NsCraft/src/World/Blocks/Block.h
@@ -1,16 +1,10 @@
 #pragma once
 #include <string>
 
-typedef std::uint8_t Block_ID;
-
 class Block {
 public:
-	Block(Block_ID id, const std::string& name);
+	Block() = default;
 	~Block() = default;
 public:
-	Block_ID getID() const;
-	const std::string& getName() const;
 private:
-	Block_ID m_id;
-	std::string m_name;
 };
