d645e43ecc621599de4b8b6cd12674859d13cae2
Nabeel Siddiqui <nabeel.siddiqui.314@gmail.com>
2020-07-15 18:52:59 +0530

    added addition and subtraction operators.

---
+++ NsCraft/src/Math/Vector3.cpp
@@ -0,0 +1,21 @@
+#include "Vector3.h"
+
+const Vector3 Vector3::operator+(const Vector3& other) const {
+	return { x + other.x, y + other.y, z + other.z };
+}
+
+const Vector3 Vector3::operator-(const Vector3& other) const {
+	return { x - other.x, y - other.y, z - other.z };
+}
+
+void Vector3::operator+=(const Vector3& other) {
+	x += other.x;
+	y += other.y;
+	z += other.z;
+}
+
+void Vector3::operator-=(const Vector3 & other) {
+	x -= other.x;
+	y -= other.y;
+	z -= other.z;
+}
--- NsCraft/src/Math/Vector3.h
+++ NsCraft/src/Math/Vector3.h
@@ -2,4 +2,10 @@
 
 struct Vector3 {
 	int x, y, z;
+public:
+	const Vector3 operator+(const Vector3& other) const;
+	const Vector3 operator-(const Vector3& other) const;
+
+	void operator+=(const Vector3& other);
+	void operator-=(const Vector3& other);
 };
