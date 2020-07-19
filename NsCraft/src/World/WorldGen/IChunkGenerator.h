7b192f73eb39002c05323aa5715f827ffa8a9f0b
Nabeel Siddiqui <nabeel.siddiqui.314@gmail.com>
2020-07-19 19:56:40 +0530

    added position parameter to chunk generator interface.

--- NsCraft/src/World/WorldGen/IChunkGenerator.h
+++ NsCraft/src/World/WorldGen/IChunkGenerator.h
@@ -2,11 +2,12 @@
 #include <memory>
 
 class Chunk;
+struct Vector3;
 
 class IChunkGenerator {
 public:
 	IChunkGenerator() = default;
 	virtual ~IChunkGenerator() = default;
 public:
-	virtual std::shared_ptr<Chunk> generateChunk() = 0;
+	virtual std::shared_ptr<Chunk> generateChunk(const Vector3& position) = 0;
 };
