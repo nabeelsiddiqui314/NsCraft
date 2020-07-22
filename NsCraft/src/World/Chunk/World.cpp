6b4592881ffb084aea6a521006542e0efa3b4a71
Nabeel Siddiqui <nabeel.siddiqui.314@gmail.com>
2020-07-23 01:29:33 +0530

    added observers to the world.

--- NsCraft/src/World/Chunk/World.cpp
+++ NsCraft/src/World/Chunk/World.cpp
@@ -1,15 +1,30 @@
 #include "World.h"
 #include "Chunk.h"
 #include "../WorldGen/IChunkGenerator.h"
+#include "IWorldObserver.h"
 
 void World::loadChunk(const Vector3& position) {
 	if (!doesChunkExist(position)) {
 		m_chunkMap.emplace(std::make_pair(position, m_chunkGenerator->generateChunk(position)));
+
+		for (auto& observer : m_observers) {
+			observer->onChunkLoad(position);
+		}
 	}
 }
 
 void World::unloadChunk(const Vector3& position) {
-	m_chunkMap.erase(position);
+	if (doesChunkExist(position)) {
+		m_chunkMap.erase(position);
+
+		for (auto& observer : m_observers) {
+			observer->onChunkUnload(position);
+		}
+	}
+}
+
+void World::addObserver(const WorldObserverPtr& observer) {
+	m_observers.emplace_back(observer);
 }
 
 bool World::doesChunkExist(const Vector3& position) const {
--- NsCraft/src/World/Chunk/World.h
+++ NsCraft/src/World/Chunk/World.h
@@ -1,21 +1,28 @@
 #pragma once
 #include <memory>
 #include <unordered_map>
+#include <vector>
 #include "../../Math/Vector3.h"
 
 class Chunk;
 class IChunkGenerator;
+class IWorldObserver;
 
 class World {
+private:
+	typedef std::shared_ptr<IWorldObserver> WorldObserverPtr;
 public:
 	World() = default;
 	~World() = default;
 public:
 	void loadChunk(const Vector3& position);
 	void unloadChunk(const Vector3& position);
+
+	void addObserver(const WorldObserverPtr& observer);
 private:
 	bool doesChunkExist(const Vector3& position) const;
 private:
 	std::unordered_map<Vector3, Chunk> m_chunkMap;
 	std::unique_ptr<IChunkGenerator> m_chunkGenerator;
+	std::vector<WorldObserverPtr> m_observers;
 };
