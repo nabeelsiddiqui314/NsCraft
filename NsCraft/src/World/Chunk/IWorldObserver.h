ac73fa696632faf726b6d4290a8dd6e34fe06314
Nabeel Siddiqui <nabeel.siddiqui.314@gmail.com>
2020-07-22 18:31:59 +0530

    removed chunk as a parameter from world observer interface methods. World observers will have a reference to world.

--- NsCraft/src/World/Chunk/IWorldObserver.h
+++ NsCraft/src/World/Chunk/IWorldObserver.h
@@ -1,14 +1,12 @@
 #pragma once
-#include <memory>
 
 struct Vector3;
-class Chunk;
 
 class IWorldObserver {
 public:
 	IWorldObserver() = default;
 	virtual ~IWorldObserver() = default;
 public:
-	virtual void onChunkLoad(const Vector3& position, const std::shared_ptr<Chunk>& chunk) = 0;
-	virtual void onChunkUnload(const Vector3& position, const std::shared_ptr<Chunk>& chunk) = 0;
+	virtual void onChunkLoad(const Vector3& position) = 0;
+	virtual void onChunkUnload(const Vector3& position) = 0;
 };
