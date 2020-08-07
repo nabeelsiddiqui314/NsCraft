cb2e8ad2423d0c3fcbcedaf061d5052b1db694d2
Nabeel Siddiqui <nabeel.siddiqui.314@gmail.com>
2020-08-07 18:45:58 +0530

    added world as an argument in the mesh generator method.

--- NsCraft/src/Rendering/Mesh/IMeshGenerator.h
+++ NsCraft/src/Rendering/Mesh/IMeshGenerator.h
@@ -2,8 +2,9 @@
 
 class ChunkMesh;
 struct Vector3;
+class World;
 
 class IMeshGenerator {
 public:
-	virtual void generateMesh(ChunkMesh& mesh, const Vector3& blockPosition) = 0;
+	virtual void generateMesh(ChunkMesh& mesh, const World& world, const Vector3& blockPosition) = 0;
 };
