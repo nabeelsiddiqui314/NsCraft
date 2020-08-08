47065c014a31b85b41b119ce3ae053cd0c650b6b
Nabeel Siddiqui <nabeel.siddiqui.314@gmail.com>
2020-08-08 21:40:43 +0530

    Block has a mesh generator.

--- NsCraft/src/World/Blocks/IBlock.h
+++ NsCraft/src/World/Blocks/IBlock.h
@@ -1,10 +1,13 @@
 #pragma once
-#include <string>
+#include <memory>
+
+class IMeshGenerator;
 
 class IBlock {
 public:
 	IBlock() = default;
 	virtual ~IBlock() = default;
 public:
+	virtual const std::unique_ptr<IMeshGenerator>& getMeshGenerator() const = 0;
 private:
 };
