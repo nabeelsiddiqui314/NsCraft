be31b4b2d9894a038cfdd057947c1d9e134ec94f
Nabeel Siddiqui <nabeel.siddiqui.314@gmail.com>
2020-08-10 15:55:29 +0530

    Added helper variables that store vectors to all 8 directions

---
+++ NsCraft/src/Math/Directions.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Vector3.h"
+
+namespace Directions {
+	const Vector3 Up    (  0,  1,  0 );
+	const Vector3 Down  (  0, -1,  0 );
+	const Vector3 Right (  1,  0,  0 );
+	const Vector3 Left  ( -1,  0,  0 );
+	const Vector3 Front (  0,  0,  1 );
+	const Vector3 Back  (  0,  0, -1 );
+}
