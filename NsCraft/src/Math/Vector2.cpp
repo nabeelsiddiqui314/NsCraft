27d1530f29d708685b2dbfc511f54d7654a2cbe0
Nabeel Siddiqui <nabeel.siddiqui.314@gmail.com>
2020-08-13 18:15:27 +0530

    simple vector2 struct added

---
+++ NsCraft/src/Math/Vector2.cpp
@@ -0,0 +1,5 @@
+#include "Vector2.h"
+
+Vector2::Vector2(int _x, int _y) 
+	: x(_x),
+      y(_y) {}
---
+++ NsCraft/src/Math/Vector2.h
@@ -0,0 +1,8 @@
+#pragma once
+
+struct Vector2 {
+	int x, y;
+public:
+	Vector2() = default;
+	Vector2(int _x, int _y);
+};
