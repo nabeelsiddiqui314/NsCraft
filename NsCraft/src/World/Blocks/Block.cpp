03b437209126f5a27b9532a003826e99326160d2
Nabeel Siddiqui <nabeel.siddiqui.314@gmail.com>
2020-08-19 20:57:55 +0530

    added an opaqueness property to blocks.

--- NsCraft/src/World/Blocks/Block.cpp
+++ NsCraft/src/World/Blocks/Block.cpp
@@ -1,8 +1,13 @@
 #include "Block.h"
 
-Block::Block(const MeshGeneratorPtr& meshGenerator) 
-	: m_meshGenerator(meshGenerator) {}
+Block::Block(const MeshGeneratorPtr& meshGenerator, bool isOpaque)
+	: m_meshGenerator(meshGenerator),
+      m_isOpaque(isOpaque) {}
 
 Block::MeshGeneratorPtr Block::getMeshGenerator() const {
 	return m_meshGenerator;
+}
+
+bool Block::isOpaque() const {
+	return m_isOpaque;
 }
--- NsCraft/src/World/Blocks/Block.h
+++ NsCraft/src/World/Blocks/Block.h
@@ -7,10 +7,12 @@ class Block {
 private:
 	typedef std::shared_ptr<IMeshGenerator> MeshGeneratorPtr;
 public:
-	Block(const MeshGeneratorPtr& meshGenerator);
+	Block(const MeshGeneratorPtr& meshGenerator, bool isOpaque);
 	~Block() = default;
 public:
 	MeshGeneratorPtr getMeshGenerator() const;
+	bool isOpaque() const;
 private:
 	MeshGeneratorPtr m_meshGenerator;
+	bool m_isOpaque;
 };
