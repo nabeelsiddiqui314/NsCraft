0f18b9112d0bb57fc96c43076596f793a5fa0fbd
Nabeel Siddiqui <nabeel.siddiqui.314@gmail.com>
2020-08-27 22:32:47 +0530

    Implemented the chunk renderer. It is temporary and must be refactored soon.

--- NsCraft/src/Rendering/ChunkRenderer.cpp
+++ NsCraft/src/Rendering/ChunkRenderer.cpp
@@ -1,13 +1,31 @@
 #include "ChunkRenderer.h"
+#include <GL/glew.h>
+#include "Mesh/ChunkMesh.h"
+#include "../OpenGL/VertexArray.h"
+#include "Camera/Camera.h"
 
-void ChunkRenderer::addMesh(const Vector3& position, const ChunkMesh& meshr) {
+ChunkRenderer::ChunkRenderer() : m_shader("shaders/basicShader.vs", "shaders/basicShader.fs") {
+	m_texture.loadFromPath("res/smiley.png");
+	m_texture.bind();
+	m_shader.useProgram();
 
+	glEnable(GL_DEPTH_TEST);
+}
+
+void ChunkRenderer::addMesh(const Vector3& position, const ChunkMesh& mesh) {
+	m_renderableChunkMap.emplace(std::make_pair(position, mesh.generateChunkVAO()));
 }
 
 void ChunkRenderer::removeMesh(const Vector3& position) {
-
+	m_renderableChunkMap.erase(position);
 }
 
-void ChunkRenderer::renderChunks() {
-
+void ChunkRenderer::renderChunks(const Camera& camera) {
+	for (const auto& [position, chunkVao] : m_renderableChunkMap) {
+		chunkVao->bind();
+		m_shader.setUniformMat4("u_view", camera.getView());
+		m_shader.setUniformMat4("u_projection", camera.getProjection());
+		glDrawElements(GL_TRIANGLES, chunkVao->getIndexCount(), GL_UNSIGNED_INT, 0);
+		chunkVao->unbind();
+	}
 }
--- NsCraft/src/Rendering/ChunkRenderer.h
+++ NsCraft/src/Rendering/ChunkRenderer.h
@@ -1,14 +1,29 @@
 #pragma once
+#include <unordered_map>
+#include <memory>
+#include "../Math/Vector3.h"
 
-struct Vector3;
+#include "../OpenGL/Texture.h"
+#include "../OpenGL/Shader.h"
+
 class ChunkMesh;
+class Camera;
+class VertexArray;
 
 class ChunkRenderer {
+private:
+	typedef std::shared_ptr<VertexArray> VaoPtr;
 public:
-	ChunkRenderer() = default;
+	ChunkRenderer();
 	~ChunkRenderer() = default;
 public:
 	void addMesh(const Vector3& position, const ChunkMesh& mesh);
 	void removeMesh(const Vector3& position);
-	void renderChunks();
+	void renderChunks(const Camera& camera);
+private:
+	std::unordered_map<Vector3, VaoPtr> m_renderableChunkMap;
+
+	// temporary
+	Texture m_texture;
+	Shader m_shader;
 };
