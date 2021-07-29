7e14d9fd2d5818b974904fe60256b30622f2b9c7
Nabeel Siddiqui <nabeel.siddiqui.314@gmail.com>
2021-07-30 01:20:43 +0530

    added array of directions for easily looping through them

--- NsCraft/src/Math/Directions.h
+++ NsCraft/src/Math/Directions.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Vector3.h"
+#include <array>
 
 namespace Directions {
 	const Vector3 Up    (  0,  1,  0 );
@@ -8,4 +9,6 @@ namespace Directions {
 	const Vector3 Left  ( -1,  0,  0 );
 	const Vector3 Front (  0,  0,  1 );
 	const Vector3 Back  (  0,  0, -1 );
+
+	const std::array<Vector3, 6> List = { Up, Down, Right, Left, Front, Back };
 }
